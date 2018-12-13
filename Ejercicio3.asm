5bc701fe092eea5d087192e7b5fb70f7efef91a2
CrysthelAparicio <CrysthelAparicio>
2018-12-13 16:15:20 -0600

    Terminado

--- Ejercicio3.asm
+++ Ejercicio3.asm
@@ -1,51 +1,74 @@
-; #########################################################################
-
       .386
-      .model flat, stdcall
-      option casemap :none   ; case sensitive
-
-; #########################################################################
-
+      .model flat,stdcall    
+      option casemap :none  
+      
       include \masm32\include\windows.inc
       include \masm32\include\user32.inc
       include \masm32\include\kernel32.inc
-
       includelib \masm32\lib\user32.lib
       includelib \masm32\lib\kernel32.lib
 
-; #########################################################################
 .data
-    Titulo    db "Letra mas repetida",0
-      Mensaje1 db "En el texto "
-      texto db "UNIVERSIDAD TECNOLÓGICA CENTROAMERICANA"
-      Mensaje2   db " la letra mas repetida es la "
-      letra db "    ",0  
-      
-      
+      titulo       db "Letra que mas se repite",0
+      mensaje db " "
+      text db "Hola amiguitos, soy Gocu",13,10
+      aux db "letra que mas se repite: "
+      resultado db "  ",0
 
 .code
-include mas_repetida.inc
-include bintotext.inc
-start:
-    mov esi,offset texto
-    mov edi,offset letra
-    call mas_repetida
+include bin2txt.inc
 
+start:
+  mov ax,0
+  mov dx,0
+  mov esi,offset text
+  mov edi,esi
+  add edi,1
+  mov ecx,offset aux
+
+  lazo:
+    cmp esi,ecx
+    jge salida
+    cmp edi,ecx
+    jge cicloaux
+
+ equal:
+    mov bh,[esi]
+    cmp bh,[edi]
+    je repetido
+    add edi,1
+    jmp lazo
+
+  repetido:
+    inc ax
+    add edi,1
+    jmp lazo
+
+  cicloaux:
+    add esi,1
+    mov edi,esi
+    add edi,1
+
+    cmp ax,dx
+    jge cicloFrecuencia
+    mov ax,0
+    jmp equal
+
+   cicloFrecuencia:
+    mov dx,ax
+    mov bl,bh
+    mov ax,0
+    jmp equal
+  
+  salida:
+    mov edi,offset resultado
+    mov [edi],bl
     push MB_OK
-    push offset Titulo
-    push offset Mensaje1
+    push offset titulo
+    push offset mensaje
     push 0
     call MessageBox
     push 0
     call ExitProcess
+
 end start
-
-    ; --------------------------------------------------------
-    ; The following are the same function calls using MASM
-    ; "invoke" syntax. It is clearer code, it is type checked
-    ; against a function prototype and it is less error prone.
-    ; --------------------------------------------------------
-
-    ; invoke MessageBox,0,ADDR szMsg,ADDR szDlgTitle,MB_OK
-    ; invoke ExitProcess,0
-
